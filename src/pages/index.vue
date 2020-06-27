b5ec306159e23217c8f50cf73d27ba5559ad4eb5
Shohei Kamiya <shoooooo.bqt@gmail.com>
2020-06-27 10:01:04 +0900

    チャートを導入

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    Hello, world
+  <div class="test">
+    <snow-fall-chart :style="chartStyle" />
   </div>
 </template>
+
+<script>
+import SnowFallChart from '@/components/SnowFallChart'
+export default {
+  components: {
+    SnowFallChart,
+  },
+  computed: {
+    chartStyle() {
+      return {
+        position: 'relative',
+        height: '100%',
+        width: '100%',
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.test {
+  margin: 0 auto;
+  width: 50%;
+}
+</style>
