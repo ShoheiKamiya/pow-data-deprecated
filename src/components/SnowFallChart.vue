ff489d454bed9236a523564a914b230f4721fdb3
ShoheiKamiya <49185403+ShoheiKamiya@users.noreply.github.com>
2020-06-27 16:53:34 +0900

    グラフをテスト導入

---
+++ src/components/SnowFallChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  data: () => ({
+    datacollection: {
+      labels: ['20', '21', '22'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#74b9ff',
+          data: [40, 20, 30],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  }),
+
+  mounted() {
+    this.renderChart(this.datacollection, this.options)
+  },
+}
+</script>
